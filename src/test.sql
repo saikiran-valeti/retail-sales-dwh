1bb20366e87bc9bf82f2d2603ae1bab3a6c25ffb
saikiran-valeti <saikiranvaleti14@gmail.com>
2026-05-06 10:23:40 +0000

    test

---
+++ src/test.sql
@@ -0,0 +1,17 @@
+USE CATALOG retail_project;
+USE SCHEMA bronze;
+
+select * from customers;
+select * from stores;
+select * from products;
+select * from sales;
+
+SELECT 'dim_customer' AS table_name, COUNT(*) AS total_rows,
+       SUM(CASE WHEN IsActive = 1 THEN 1 ELSE 0 END) AS active_rows
+FROM retail_project.silver.dimcustomer
+UNION ALL
+SELECT 'dim_product', COUNT(*), NULL FROM retail_project.silver.dimproduct
+UNION ALL
+SELECT 'dim_store',   COUNT(*), NULL FROM retail_project.silver.dimstore
+UNION ALL
+SELECT 'fact_sales',  COUNT(*), NULL FROM retail_project.gold.factsales;
